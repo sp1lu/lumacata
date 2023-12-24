e207bc9fa556aba992c7d20a063bf6f42d257f0f
sp1lu <davide.rivolta@yahoo.it>
2023-12-24 23:05:41 +0100

    Index page component set

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -7,7 +7,25 @@ export class Index extends HTMLElement {
     }
 
     connectedCallback() {
-        this.shadow.innerHTML = '<div>INDEX</div>';
+        // html
+        this.shadow.innerHTML =
+            `
+            <div>
+                <h1>
+                    <span>LUMA</span>
+                    <span>CATA</span>
+                    <span>2024</span>
+                </h1>
+                <h2>10 - 15 giugno</h2>
+            </div>
+            `
+            ;
+
+        // css
+        const style = document.createElement('link');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', './css/index.css');
+        this.shadow.append(style);
     }
 }
 
